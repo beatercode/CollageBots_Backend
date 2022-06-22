ed13d29e0097e326c27b7672044cac7a851d093e
Luca Minoi <beater@MBP-di-Luca.wind3.hub>
2022-06-22 19:25:45 +0200

    test htttps

--- backend.js
+++ backend.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 443;
+const port = 3010;
 
 app.use(cors());
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
